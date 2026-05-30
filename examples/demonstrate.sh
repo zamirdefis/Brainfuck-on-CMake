2794cd073cbc34e793c84170530ba8deb95e0989
loksuf <zay.mir2008@gmail.com>
2026-05-30 21:04:22 +0300

    The output system in code_view has been modified

--- examples/demonstrate.sh
+++ examples/demonstrate.sh
@@ -8,8 +8,10 @@ function run {
   cmake -G "Ninja Multi-Config" -S ../src -B ../build "$@"
 }
 
-# run -Dsource="${source_dir}/hellOwOrld.bf"
-# run -Dsource="${source_dir}/char.bf"
-run -Dsource="${source_dir}/mv_cell.bf"
+run -Dsource="${source_dir}/hellOwOrld.bf"
+run -Dsource="${source_dir}/char.bf"
+run -Dsource="${source_dir}/mv_cell.bf" -Ddebug=ON -Dinput="a"
+# run -Dsource="../examples/triangle.bf" -Ddebug=ON -Dcode_view=OFF -Dsize=1024 # On my PC, this took 136 seconds :O
+# run -Dsource="../examples/mandelbrot.bf" -Ddebug=ON -Dcode_view=OFF -Dsize=1024 # Don't even try X_X
 
 cd - || exit 2
